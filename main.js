e970943643b095d485460d4f5d2ada72466ef408
ArunRaj2312 <arunvijay1043534@gmail.com>
2022-12-13 18:06:16 -1200

    add file

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 let a=parseInt(prompt("enter the length"));
 let b;
 let c=0;
-for(i=1;i<=a;i++){
+let i=1;
+while(i<=a){
    b=parseInt(prompt("enter the value"));
    document.write(b+" ")
    if(b>0){
     c+=b;
    }
+   i++
 }
 document.write("<br><br>")
 document.write(c);
